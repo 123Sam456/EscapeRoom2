00b58a8730ca4fe3e28f5a7652b21d8a68ef6c39
sam <epics@Sams-labtop>
2024-11-06 20:43:05 -0600

    GetUserInput method done

--- TextGame/src/def/Main.java
+++ TextGame/src/def/Main.java
@@ -25,50 +25,58 @@ public class Main {
 	}
 	
 	// gets user number in the range of 1 to selectedRange 
-	/*public static int GetUserInput(int selectRange){
-		boolean condition = true;
-		int input = -1;
-		while (condition) {
-			try (Scanner myObj = new Scanner(System.in)) {
-				if(myObj.hasNextInt()) {
-					// Read user input
-					input = myObj.nextInt();
-					if(input < selectRange && input >= 1) {
-						condition = false;	
-					}
-					else {
-						System.out.println("you inputed an invalid number ");
-					}
-				}
-			}
-		}
-		return input;
-	}*/
-	
 	public static int GetUserInput(int selectRange){
 		Scanner myObj = new Scanner(System.in);
 			// Read user input
-			int input;
-			
+			int input = -1;
 			while (true) {
-			if(myObj.hasNextInt()) {
-				input = myObj.nextInt();
-			// checks if the users input is with in the parameters
-			// if yes break the while loop and return the user input
-			if(input <= selectRange && input >= 1) {
-				break;
-			}
-			else {
-			// gets the users next input and checks if it is good
-				continue;
-				//input = myObj.nextInt();//continue;
-			}
+				String a = myObj.nextLine();
+				// Checks if the user inputed a number
+				if(isInteger(a)) {
+				input = Integer.parseInt(a);
+					// checks if the users input is with in the parameters
+					// if yes break the while loop and return the user input
+					if(input <= selectRange && input >= 1) {
+						break;
+					}
+					else {
+						// gets the users next input and checks if it is good
+						System.out.println("You didn't input a number in the range you were given");
+						continue;
+					}
 				}
-			else {
-				continue;
-			}
+				else {
+					// If the user didn't input a number
+					System.out.println("You didn't input a number");
+					continue;
+				}
 			}
 			return input;
 		}
+	
+	
+	public static boolean isInteger(String str) {
+	    if (str == null) {
+	        return false;
+	    }
+	    int length = str.length();
+	    if (length == 0) {
+	        return false;
+	    }
+	    int i = 0;
+	    if (str.charAt(0) == '-') {
+	        if (length == 1) {
+	            return false;
+	        }
+	        i = 1;
+	    }
+	    for (; i < length; i++) {
+	        char c = str.charAt(i);
+	        if (c < '0' || c > '9') {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
+}
 
