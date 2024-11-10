4fd38138e756919a018c3740b19d7cc4c28b06a9
Enkheln <149412901+Enkheln@users.noreply.github.com>
2024-11-10 16:18:18 -0600

    EndingClass - comment

--- TextGame/src/def/Keys.java
+++ TextGame/src/def/Keys.java
@@ -6,9 +6,10 @@ public class Keys {
 	public static boolean Room3Key = false;
 	public static boolean Room4Key = false;
 
-	// Treemap
+	// Treemap to store
 	static TreeMap<String, Boolean> hintCount = new TreeMap<>();
 
+	// Set all the hints to false at the beginning of the game.
 	static {
 		hintCount.put("Hint 1", false);
 		hintCount.put("Hint 2", false);
@@ -18,22 +19,24 @@ public class Keys {
 }
 
 public class Ending {
+
+	// Method to determine ending depending on the hints used.
 	public static void EndGame() {
 
-		// Convert hints to an array
+		// Convert treemap to an array to determine if hints have been used or not.
 		Boolean[] hintArray = Keys.hintCount.values().toArray(new Boolean[0]);
 
-		// Initialize the counter
+		// Initialize the counter to see how many hints have been used.
 		int usedHintCount = 0;
 
-		// for loop
+		// loop through hintArray to count the hints used.
 		for (int i = 0; i < hintArray.length; i++) {
 			if (hintArray[i]) {
 				usedHintCount++;
 			}
 		}
 
-		// Outcome
+		// Display message depending on how many hints used.
 		if (usedHintCount == 0) {
 			System.out.println("No hint");
 		} else if (usedHintCount >= 1 && usedHintCount <= 3) {
@@ -43,7 +46,6 @@ public class Ending {
 		} else {
 			System.out.println("Do nothing");
 		}
-
 		System.out.println("Thank you for playing!");
 
 	}
