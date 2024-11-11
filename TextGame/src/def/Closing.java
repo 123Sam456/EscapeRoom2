17c8b665d81a049a6d423c631cfe696665bb015e
Enkheln <149412901+Enkheln@users.noreply.github.com>
2024-11-10 19:29:11 -0600

    EndingClass - fixed

--- TextGame/src/def/Closing.java
+++ TextGame/src/def/Closing.java
@@ -1,36 +1,34 @@
 package def;
 
-public class Closing {
+public class Ending {
 
-	// Method to determine ending depending on the hints used.
-	public static void EndGame() {
+    public static void EndGame() {
 
-		Keys keyClass = new Keys();
-		
-		// Convert treemap to an array to determine if hints have been used or not.
-		Boolean[] hintArray = keyClass.hintCount.values().toArray(new Boolean[0]);
+        // Convert hints to an array
+        Boolean[] hintArray = Keys.hintCount.values().toArray(new Boolean[0]);
 
-		// Initialize the counter to see how many hints have been used.
-		int usedHintCount = 0;
+        // Initialize the counter
+        int usedHintCount = 0;
 
-		// loop through hintArray to count the hints used.
-		for (int i = 0; i < hintArray.length; i++) {
-			if (hintArray[i]) {
-				usedHintCount++;
-			}
-		}
+        // for loop
+        for (int i = 0; i < hintArray.length; i++) {
+            if (hintArray[i]) {
+                usedHintCount++;
+            }
+        }
 
-		// Display message depending on how many hints used.
-		if (usedHintCount == 0) {
-			System.out.println("No hint");
-		} else if (usedHintCount >= 1 && usedHintCount <= 3) {
-			System.out.println("Some hints");
-		} else if (usedHintCount == 4) {
-			System.out.println("All hint");
-		} else {
-			System.out.println("Do nothing");
-		}
-		System.out.println("Thank you for playing!");
+        // Outcome
+        if (usedHintCount == 0) {
+            System.out.println("No hint");
+        } else if (usedHintCount >= 1 && usedHintCount <= 3) {
+            System.out.println("Some hints");
+        } else if (usedHintCount == 4) {
+            System.out.println("All hint");
+        } else {
+            System.out.println("Do nothing");
+        }
 
-	}
+        System.out.println("Thank you for playing!");
+
+    }
 }
